864b40856761674fe5ffeccf5e41b463c13f19b7
sveta-maker <svetadevil32@gmail.com>
2023-12-28 00:23:58 +0200

    Injected more weather data from API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
-     let cityElement = document.querySelector("#city");
-    let temperature=response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let temperature = response.data.temperature.current;
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
  
     temperatureElement.innerHTML = Math.round(temperature);       
-     cityElement.innerHTML = response.data.city;
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+
+    let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+    ];
+    let day = days[date.getDay()];
+    if (minutes < 10) { minutes = `0${minutes}`; }
+
+    return `${day} ${hours}:${minutes}`;
+
+}
 
 function searchCity(city) {
     let apiKey = "da3b4f2006boa20498f58408839atf92";
