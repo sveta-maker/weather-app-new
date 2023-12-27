11e383aaea7bcfabed7ac4d86c0137a6714a43d3
sveta-maker <svetadevil32@gmail.com>
2023-12-27 23:49:05 +0200

    Added API integration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+     let cityElement = document.querySelector("#city");
+    let temperature=response.data.temperature.current;
+ 
+    temperatureElement.innerHTML = Math.round(temperature);       
+     cityElement.innerHTML = response.data.city;
+}
+
+
+function searchCity(city) {
+    let apiKey = "da3b4f2006boa20498f58408839atf92";
+    let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    console.log(searchInput);
-    let cityElement = document.querySelector("#city");
-    console.log(cityElement);
-    let enteredCity = searchInput.value;
- console.log(enteredCity);
-    cityElement.innerHTML = enteredCity;
-   
+    searchCity(searchInput.value);
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
