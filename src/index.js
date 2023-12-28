9a8306de8b56809fdd4c56829470f525cfb530a6
sveta-maker <svetadevil32@gmail.com>
2023-12-29 01:07:25 +0200

    Added weather forecast API integration- realtime day, temperature,icon

--- src/index.js
+++ src/index.js
@@ -60,6 +60,12 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[date.getDay()];
+}
+
 function getForecast(city) {
     let apiKey = "da3b4f2006boa20498f58408839atf92";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -68,26 +74,28 @@ function getForecast(city) {
 
 function displayForecast(response) {
     
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    
     let forecastHtml = "";
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+        if (index<5){
         forecastHtml =
             forecastHtml +
          `
     <div class="row">
         <div class="col-2">
             <div class="weather-forecast-date">
-                ${day}
+                ${formatDay(day.time)}
             </div>
 
-            <img src="http://openweathermap.org/img/wn/50d@2x.png" width="42" />
+            <img src="${day.condition.icon_url}" width="88" />
             <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperature-max">18°</span>
-                <span class="weather-forecast-temperature-min">12°</span>
+                <span class="weather-forecast-temperature-max">${Math.round(day.temperature.maximum)}°</span>
+                <span class="weather-forecast-temperature-min">${Math.round(day.temperature.minimum)}°</span>
             </div>
         </div>
     </div>
     `;
+      }      
     })
     forecastElement.innerHTML = forecastHtml;
 
